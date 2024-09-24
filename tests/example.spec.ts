ee35e8a05ff07e92be7e3e95668ed78d3196b2fc
sureshnanjan <sureshnanjan@outlook.com>
2024-09-24 17:32:01 +0530

    Playwright Setup with Demo Tests

---
+++ tests/example.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+
+test('Heroku Page has correct Title', async ({ page }) => {
+  // Open the browser and navigate to Heroku App homepage
+  await page.goto('https://the-internet.herokuapp.com/');
+
+  // Read the title and check if it matched "The Internet"
+  
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/The Internet/);
+});
+
+test("Heroku HomePage Heading is correct", async ({page})=>{
+  // Open the Heroku App Home Page
+  await page.goto('https://the-internet.herokuapp.com/');
+  // read the heading
+  const expectedHeading = "Welcome to the-internet"
+  const actualHeading = await page.getByRole('heading', { name: 'Welcome to the-internet' }).textContent()
+  // check whther the heading matched 
+  await expect(actualHeading).toEqual(expectedHeading)
+
+})
+
+
