f1d7e517c66dfa9956aab00e065567b19df16531
Ajitkumar1923 <1000067061@hexaware.com>
2024-09-29 23:50:30 +0530

    Rename Heroku with ABTesting

--- tests/abtesting.spec.ts
+++ tests/abtesting.spec.ts
@@ -4,7 +4,7 @@ import { AbTestingOperationP } from '../heroku-playwright/abtesting';
 import { equal } from 'assert';
 const ab_page:AbTestingOperations=new AbTestingOperationP()
 
-test("Heroku Title is OK", async () =>{
+test("AbTesting Title is OK", async () =>{
 
     const actualTitle = ab_page.getTitle();
     const expectedTitle = "The Internet"
@@ -12,7 +12,7 @@ test("Heroku Title is OK", async () =>{
 
 });
 
-test("Heroku Header Text is OK",async()=>
+test("AbTesting Header Text is OK",async()=>
 {
     const actualHeader =ab_page.getHeader();
     const expectedHeader="A/B Test Control"
@@ -20,28 +20,28 @@ test("Heroku Header Text is OK",async()=>
 
 });
 
-test("Heoku Paragrpah Text is Ok",async()=>
+test("AbTesting Paragrpah Text is Ok",async()=>
 {
     const actualParagraph  =ab_page.getParagraph();
     const expectedParagraph="Also known as split testing. This is a way in which businesses are able to simultaneously test and learn different versions of a page to see which text and/or functionality works best towards a desired outcome (e.g. a user action such as a click-through)."
     expect(actualParagraph).toEqual(expectedParagraph)
 });
 
-test("Heoku PoweredBy Selenium Link is Ok",async()=>
+test("AbTesting PoweredBy Selenium Link is Ok",async()=>
 {
     const actualPoweredLink =ab_page.getPoweredBySeleniumLink();
     const expectedPoweredLink="http://elementalselenium.com/"
     expect(actualPoweredLink).toEqual(expectedPoweredLink)
 });
 
-test("Heoku Text Font is Ok",async()=>
+test("AbTesting Text Font is Ok",async()=>
 {
     const actualTextFont =ab_page.getSize();
     const expectedTextFont="Arial"
     expect(actualTextFont).toEqual(expectedTextFont)     
 });
 
-test("Heoku Text Font Size is Ok",async()=>
+test("AbTesting Text Font Size is Ok",async()=>
 {
 
     const actualFontSize =ab_page.getSize();
